8f16789c9e84da61c9a1bc6008934f05f96d4b84
kjhrthp <qw719898@gmail.com>
2024-11-26 13:50:10 +0900

    branch_commit_pyrosim

--- generate.py
+++ generate.py
@@ -1,5 +1,49 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
-pyrosim.Send_Cube(name="Box2", pos=[1,0,1.5] , size=[1,1,1])
-pyrosim.End()
+
+def create_world():
+    pyrosim.Start_SDF("world.sdf")  # SDF 파일 생성 시작
+    pyrosim.Send_Cube(pos=[0, 0, 0], size=[1, 1, 1])  # 단일 블록 추가
+    pyrosim.End()  # SDF 파일 저장
+
+
+
+def create_robot():
+    # URDF 파일 생성 시작
+    pyrosim.Start_URDF("body.urdf")
+
+    # Torso (루트 링크) 추가 - 절대 좌표
+    pyrosim.Send_Cube(name="Torso",
+                      pos=[0, 0, 0.5],
+                      size=[1, 1, 1])
+
+    # Torso와 Leg을 연결하는 Joint 추가 - 절대 좌표
+    pyrosim.Send_Joint(name="Torso_R_Leg",
+                       parent="Torso", child="R_Leg",
+                       type="fixed",
+                       position=[0.5, 0, 1.5]
+                       )
+
+    # Leg 추가 - Joint 기준 상대 좌표
+    pyrosim.Send_Cube(name="R_Leg",
+                      pos=[0.5, 0, 0],
+                      size=[1, 1, 1])
+
+    # Torso와 Leg을 연결하는 Joint 추가 - 절대 좌표
+    pyrosim.Send_Joint(name="Torso_L_Leg",
+                       parent="Torso",
+                       child="L_Leg",
+                       type="fixed",
+                       position=[-0.5, 0, 1.5])
+
+    # Leg 추가 - Joint 기준 상대 좌표
+    pyrosim.Send_Cube(name="L_Leg",
+                      pos=[-0.5, 0, 0],
+                      size=[1, 1, 1])
+
+    # URDF 파일 저장 종료
+    pyrosim.End()
+
+
+
+create_world()
+create_robot()
--- simulation.py
+++ simulation.py
@@ -4,8 +4,8 @@ physicsClient = p.connect(p.GUI)
 p.setRealTimeSimulation(1)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
-planeId = p.loadURDF("plane.urdf")
-p.loadSDF("boxes.sdf")
+p.loadURDF("plane.urdf")
+planeId = p.loadURDF("body.urdf")
 import time
 for i in range(1000):
     p.stepSimulation()
