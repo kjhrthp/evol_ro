6265ae1227efdd0eaa003dd4b3e5d8cf99e05a3c
KJH <qw719898@gmial.com>
2025-01-13 08:44:01 +0900

    master push after refactoring

--- generate.py
+++ generate.py
@@ -1,4 +1,13 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
-pyrosim.End()
+
+def create_robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0.5], size=[1, 1, 1])
+    # should be named "Parent_Child"
+    pyrosim.Send_Joint(name = "Torso_Leg", parent="Torso",
+                       child="Leg", type="revolute", position=[0.5,0,1])
+    # child로 지정된 Leg는 joint의 상대위치이다.
+    pyrosim.Send_Cube(name="Leg", pos=[0.5, 0, 0.5], size=[1, 1, 1])
+    pyrosim.End()
+
+create_robot()
--- simulation.py
+++ simulation.py
@@ -1,6 +1,10 @@
 import pybullet as p
+import pybullet_data
 physicsClient = p.connect(p.GUI)
-p.loadSDF("box.sdf")
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.81)
+planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
 import time
 for i in range(1000):
     p.stepSimulation()
